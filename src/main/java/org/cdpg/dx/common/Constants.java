8fa02defa2f6637b55b0ba016aa5e505f15ff0a7
gopal-mahajan <gopal.mahajan@datakaveri.org>
2025-04-16 16:04:34 +0530

    fix

--- src/main/java/org/cdpg/dx/common/Constants.java
+++ src/main/java/org/cdpg/dx/common/Constants.java
@@ -2,4 +2,5 @@ package org.cdpg.dx.common;
 
 public class Constants {
   public static final String PG_SERVICE_ADDRESS = "org.cdpg.dx.database.postgres";
+  public static final String ELASTIC_SERVICE_ADDRESS ="org.cdpg.dx.database.elastic";
 }
