4e3a6fd0cc5837720c941d1b70315f60c22cb363
ananjaykumar2 <ananjaykumar2@gmail.com>
2025-04-26 21:24:28 +0530

    initial commit for controller

---
+++ src/main/java/org/cdpg/dx/rs/apiserver/IngestionAdaptorController.java
@@ -0,0 +1,9 @@
+package org.cdpg.dx.rs.apiserver;
+
+import io.vertx.ext.web.openapi.RouterBuilder;
+
+public class IngestionAdaptorController implements ApiController {
+
+  @Override
+  public void register(RouterBuilder builder) {}
+}
