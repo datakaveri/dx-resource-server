470e45c2b2b863983c7152fb159621dfac7f4e76
Gopal Mahajan <gopal.mahajan@datakaveri.org>
2025-05-20 15:30:27 +0530

    hot-fix

--- src/main/java/org/cdpg/dx/database/postgres/util/DxPgExceptionMapper.java
+++ src/main/java/org/cdpg/dx/database/postgres/util/DxPgExceptionMapper.java
@@ -1,7 +1,9 @@
 package org.cdpg.dx.database.postgres.util;
 
 import io.vertx.pgclient.PgException;
-import org.cdpg.dx.common.exception.*;
+import org.cdpg.dx.common.exception.DxPgException;
+import org.cdpg.dx.common.exception.InvalidColumnNameException;
+import org.cdpg.dx.common.exception.UniqueConstraintViolationException;
 
 public class DxPgExceptionMapper {
     public static DxPgException from(Throwable t) {
@@ -16,3 +18,13 @@ public class DxPgExceptionMapper {
         return new DxPgException("Unknown DB Error", t);
     }
 }
+//
+//23505: Unique violation (duplicate key)
+//23503: Foreign key violation
+//42703: Undefined column (invalid column name)
+//23502: Not null violation
+//23514: Check constraint violation
+//42601: Syntax error
+//42P01: Undefined table
+//40001: Serialization failure (deadlock)
+//22001: String data, right truncation
